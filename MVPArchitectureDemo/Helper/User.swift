224935d6be821691e12ea7d8e91d2bd2787fd146
Bhagyashree Khatri <bhagya.khatri@webonline.com.my>
2019-08-09 21:00:58 +0800

    inital commit

---
+++ MVPArchitectureDemo/Helper/User.swift
@@ -0,0 +1,17 @@
+//
+//  User.swift
+//  MVPArchitectureDemo
+//
+//  Created by Bhagyashree Haresh Khatri on 09/08/2019.
+//  Copyright © 2019 Bhagyashree Haresh Khatri. All rights reserved.
+//
+
+import Foundation
+
+struct User {
+
+    let firstName: String
+    let lastName: String
+    let email: String
+    let age: Int
+}
